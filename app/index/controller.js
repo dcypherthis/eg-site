f4ff634c613ffdb6d6711668b132e569b31e9fe4
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-02 18:24:10 -0500

    Home hero

---
+++ app/index/controller.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  promoVideo: `https://player.vimeo.com/external/154806929.hd.mp4?s=f29b73b5250444394d91e86770273541cf62399d&profile_id=119`,
+});
---
+++ tests/unit/helpers/latest-test.js
@@ -0,0 +1,10 @@
+import { latest } from 'embergrep-site/helpers/latest';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | latest');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = latest([42]);
+  assert.ok(result);
+});

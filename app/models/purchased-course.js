b983a966a7d0841924980ff4a4b3d28e43b1d9db
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-29 11:01:59 -0400

    Purchased course model

---
+++ app/models/purchased-course.js
@@ -0,0 +1,17 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr(),
+  price: attr(),
+  time: attr(),
+  active: attr(),
+  releaseDate: attr(),
+  description: attr(),
+  longDescription: attr(),
+  comingSoon: attr(),
+  purchased: attr(),
+  lessons: hasMany('lesson'),
+  purchase: belongsTo('purchase')
+});

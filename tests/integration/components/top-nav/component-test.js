798d6822b9021b4f04cd6a232de2130be6b6edc5
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-31 16:49:00 -0400

    Top nav integration test

--- tests/integration/components/top-nav/component-test.js
+++ tests/integration/components/top-nav/component-test.js
@@ -1,24 +1,23 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
+import whitespace from '../../../helpers/consistent-whitespace';
+
 moduleForComponent('top-nav', 'Integration | Component | top nav', {
-  integration: true
+  integration: true,
 });
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
   this.render(hbs`{{top-nav}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#top-nav}}
-      template block text
-    {{/top-nav}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(
+    whitespace(this.$().text()), whitespace(`
+      >eg
+      >eg
+      home
+      signup
+      login`));
 });

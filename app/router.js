24995e310fb8dcc3e5a9bba8d55efa423cc97d64
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-31 13:42:44 -0400

    Link to lesson page for course lesson

--- app/router.js
+++ app/router.js
@@ -5,30 +5,34 @@ const Router = Ember.Router.extend({
   location: config.locationType,
 });
 
-Router.map(function routeMap() {
-  this.route(`free-lessons`, { path: `free-lesson/:free_lesson_id` });
+/* eslint array-callback-return: 0 */
+Router.map(function () {
+  this.route('free-lessons', { path: 'free-lesson/:free_lesson_id' });
 
-  this.route(`course-previews`, function coursePreview() {
-    this.route(`detail`, { path: `/:course_abstract_id` });
+  this.route('course-previews', function coursePreview() {
+    this.route('detail', { path: '/:course_abstract_id' });
   });
 
-  this.route(`signup`);
-  this.route(`login`);
-  this.route(`forgot-password`);
+  this.route('signup');
+  this.route('login');
+  this.route('forgot-password');
 
-  this.route(`license`);
+  this.route('license');
 
-  this.route('account', function() {
+  this.route('account', function () {
     this.route('purchases');
   });
 
-  this.route(`reset-password`, { path: `password/reset` });
+  this.route('reset-password', { path: 'password/reset' });
 
   this.route('course-purchase', {
-    path: '/course-purchase/:course_abstract_id'
+    path: '/course-purchase/:course_abstract_id',
   });
-  this.route('courses', function() {
-    this.route(`detail`, { path: `/:purchased_course_id` });
+
+  this.route('courses', function () {
+    this.route('detail', { path: '/:purchased_course_id' }, function () {
+      this.route('lesson', { path: '/lessons/:lesson_id' });
+    });
   });
 });
 

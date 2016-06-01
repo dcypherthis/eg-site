c97c6a9402d39ce7800a2dbf8670b520daa21ddf
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-31 21:21:17 -0400

    Use sauce testem.js not testem.json

--- testem.js
+++ testem.js
@@ -2,7 +2,10 @@
 /* eslint-disable */
 
 var launchInCI = function() {
+  console.log('TRAVIS', process.env.TRAVIS);
+  console.log('TRAVIS_PULL_REQUEST', process.env.TRAVIS_PULL_REQUEST);
   if (!process.env.TRAVIS || process.env.TRAVIS_PULL_REQUEST) {
+    process.env.TRAVIS_PULL_REQUEST && console.log('Pull request');
     return ['PhantomJS'];
   } else {
     return ['SL_chrome', 'SL_firefox'];

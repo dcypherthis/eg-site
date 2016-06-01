3633181c7dcfd6a3250f8184a520e52cafb72b16
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-31 21:16:06 -0400

    Try to use sauce labs?

--- testem.js
+++ testem.js
@@ -1,13 +1,63 @@
-/*jshint node:true*/
+/* jshint node:true */
+/* eslint-disable */
+
+var launchInCI = function() {
+  if (!process.env.TRAVIS || process.env.TRAVIS_PULL_REQUEST) {
+    return ['PhantomJS'];
+  } else {
+    return ['SL_chrome', 'SL_firefox'];
+  }
+};
+
+
 module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
-  ],
+  "launch_in_ci": launchInCI(),
   "launch_in_dev": [
     "PhantomJS",
     "Chrome"
-  ]
+  ],
+  "launchers": {
+    "SL_chrome": {
+      "exe": "./node_modules/.bin/ember-cli-sauce",
+      "args": [
+        "-b",
+        "chrome",
+        "-p",
+        "Windows 8",
+        "--at",
+        "--no-ct",
+        "--u"
+      ],
+      "protocol": "browser"
+    },
+    "SL_firefox": {
+      "exe": "./node_modules/.bin/ember-cli-sauce",
+      "args": [
+        "-b",
+        "firefox",
+        "-p",
+        "Windows 8",
+        "--at",
+        "--no-ct",
+        "--u"
+      ],
+      "protocol": "browser"
+    },
+    "SL_safari": {
+      "exe": "./node_modules/.bin/ember-cli-sauce",
+      "args": [
+        "-b",
+        "safari",
+        "-p",
+        "Mac 10.10",
+        "--at",
+        "--no-ct",
+        "--u"
+      ],
+      "protocol": "browser"
+    }
+  }
 };
